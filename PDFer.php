6435fee3fec82eaddce00b76929d50c5355b07a4
PirogovDE <d.pirogov@indicator-lab.ru>
2024-01-25 13:13:24 +0700

    solved classes

---
+++ PDFer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app;
+
+use Dompdf\Dompdf;
+
+class PDFer
+{
+    private array $content = [];
+    public Dompdf $dompdf;
+
+    public function __construct()
+    {
+        $this->dompdf = new Dompdf();
+    }
+
+    public function getContentRecursively(string $path): bool
+    {
+        $files = scandir($path);
+        foreach ($files as $file) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+            if (is_dir($path . '/' . $file)) {
+                $relativePath = $path . '/' . $file;
+                $this->getContentRecursively($relativePath);
+            } else {
+                $content = file_get_contents($path . '/' . $file);
+                $contentClean = str_replace("<?php", "", $content);
+                array_push($this->content, "<h3>$file</h3>" . $contentClean);
+            }
+        }
+        return true;
+    }
+
+    public function implodeContentWithHtml(string $html): string
+    {
+        return implode($html, $this->content);
+    }
+}
--- index.php
+++ index.php
@@ -1,35 +1,16 @@
 <?php
+
+use app\PDFer;
+
 require_once "vendor/autoload.php";
 
-use Dompdf\Dompdf;
+$defaultPath = "FilesToConvert";
 
-$dompdf = new Dompdf();
-$dirName = "FilesToConvert";
-$pages = [];
-function getContentRecursively($relativePath, $dompdf, &$pages)
-{
-    $files = scandir($relativePath);
-    foreach ($files as $file) {
-        if ($file === '.' || $file === '..') {
-            continue;
-        }
-        if (is_dir($relativePath . '/' . $file)) {
-            $path = $relativePath . '/' . $file;
-            getContentRecursively($path, $dompdf, $pages);
-        } else {
-            $content = file_get_contents($relativePath . '/' . $file);
-            $contentClean = str_replace("<?php", "", $content);
-            array_push($pages, "<h3>$file</h3>" . $contentClean);
-        }
-    }
-}
+$pdfer = new PDFer();
 
-function implodePages($pages)
-{
-    return implode("<br>", $pages);
-}
+$pdfer->getContentRecursively($defaultPath);
 
-getContentRecursively($dirName, $dompdf, $pages);
-$dompdf->loadHtml('<pre>' . implodePages($pages) . '<pre>');
-$dompdf->render();
-$dompdf->stream();
+$pdfer->dompdf->loadHtml('<pre>' . $pdfer->implodeContentWithHtml("<br>"));
+
+$pdfer->dompdf->render();
+$pdfer->dompdf->stream();

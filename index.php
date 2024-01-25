799632a78781ccbf03b01485235007a4a8451bd1
PirogovDE <d.pirogov@indicator-lab.ru>
2024-01-25 13:58:34 +0700

    custom page size

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $pdfer = new PDFer();
 $pdfer->getContentRecursively($defaultPath);
 
 $pdfer->dompdf->loadHtml('<pre>' . $pdfer->implodeContentWithHtml("<br>"));
-
+//                                  width height
+$pdfer->dompdf->setPaper(array(0, 0, 1000, 1400));
 $pdfer->dompdf->render();
 $pdfer->dompdf->stream();
